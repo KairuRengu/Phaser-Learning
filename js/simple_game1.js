5f32b2953162e8557d041a710ff8acfa1b0d4016
Kyle Leng <hyle200@gmail.com>
2018-06-06 22:57:30 -0400

    Put the player sprite loading in preload instead of create

--- js/simple_game1.js
+++ js/simple_game1.js
@@ -17,21 +17,47 @@ gameScene.preload = function(){
   this.load.image('dragon', 'assets/dragon.png');
   this.load.image('player', 'assets/player.png');
   this.load.image('treasure', 'assets/treasure.png');
-  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
-  this.player.setScale(0,5);
+
 }
 
 
 gameScene.create = function(){
+
+  //create pplayer and background
+  this.add.sprite(180,180, 'background');
   let background = this.add.sprite(0,0, 'background');
   background.setOrigin(0,0);
-  this.add.sprite(180,180, 'background');
   this.add.sprite(0,0, 'player');
-  this.add.sprite(100,200, 'treasure');
+  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
+  this.player.setScale(0,5);
+  //create the treasure
+  this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
+  this.treasure.setScale(0.6);
+  //create enemies
+  this.enemies = this.add.group({
+    key:'dragon',
+    repeat: 5,
+    setXY: {
+      x: 110,
+      y: 100,
+      stepX: 80,
+      stepY: 20
+    }
+
+  })
 }
 
 gameScene.update = function(){
+  //if pointer is pressed down move in that direction
   if(this.input.activePointer.isDown){
     this.player.x += this.playerSpeed;
+  }
+  //call gameover when the player collides with a treasure
+  if(Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())){
+    this.gameOver();
+  }
+  //this restarts the game
+  gameScene.gameOver = function(){
+    this.scene.restart();
   }
 }
